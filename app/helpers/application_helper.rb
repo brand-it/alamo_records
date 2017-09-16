40abcb7fcb00d84b423dffef755c3ba0d803237b
Brandt Lareau <brandt.lareau@rackspace.com>
2017-09-16 14:11:01 -0500

    Ran the code clean tools and started adding templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  # TODO: Finish this please
+  # def error_summary(object)
+  # end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -14,5 +16,8 @@ module AlamoRecords
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine :slim
+    end
   end
 end
